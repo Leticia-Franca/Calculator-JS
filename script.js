ce786515adbb8d2b04d444584ee455dc15f6c996
Leticia <canumsang@gmail.com>
2022-08-20 14:00:36 -0300

    adds event listeners to each digit - numeric or operator [REQUISITS TO VERIFY THE CALCULUS]

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ else
 	console.log("nao eh operador!");
 
 // We need each digit to listen to the 'click' event
+digits.forEach(digit => {
+	digit.addEventListener('click', (e) => {
+		let clickedDigit = e.target;
+		console.log(clickedDigit.textContent);
+		/*
+		 * avaliar se o digito é numérico ou um operador
+		 * o que precisa ser verificado:
+		 * se for um digito:
+		 *  - é o primeiro digito ou o segundo?
+		 *  - se for o segundo, foi precedido por um operador? se não, precisa ser
+		 * se for um operador:
+		 *  - foi precedido de digito? se não, precisa ser
+		*/
+		
+	})
+})
